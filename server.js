972c34b6039bd50327c308638d562706d11e0da9
Ruslan Nizamov <nizamoff.rus@gmail.com>
2023-01-09 15:30:45 +0100

    added a middleware example

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 //Route files
 const bootcamps = require('./routes/bootcamps');
@@ -9,12 +10,18 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+//Dev logging middlware
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,
+app.listen(
+    PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
 
