d9719338cb6653b2fd2e2659a43b5fd84164eb61
Ruslan Nizamov <nizamoff.rus@gmail.com>
2023-01-10 15:15:12 +0100

    connected MongoDB and created the Bootcamps model

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-//Route files
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -20,8 +25,15 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
     PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise  rejections
+process.on('unhandled rejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    //Close server and & exit process
+    server.close(() => process.exit(1));
+});
 
